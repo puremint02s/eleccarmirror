c0847d3adf4af43ca6998f4d8c1dec78657b1e70
evelyn <alrxltkfkd@gmail.com>
2022-11-25 21:10:50 +0900

    [Style] db 폴더 구조 수정

--- Back/index.js
+++ Back/index.js
@@ -1,5 +1,4 @@
 const app = require("./src/app");
-require("./src/db");
 
 app.listen(3000, () => {
     console.log("✅Server is running on port 3000");
--- Back/src/app.js
+++ Back/src/app.js
@@ -1,4 +1,20 @@
 const express = require("express");
+const { sequelize } = require("./db/models");
+
+const ConnectDB = async() => {
+    try {
+      await sequelize.authenticate();
+      console.log("✅DB 연결 완료");
+  
+      await sequelize.sync({force: false});
+      console.log("✅DB 동기화 완료")
+  
+    } catch(err) {
+      console.log("⛔DB 연결 및 동기화 실패", err)
+    }
+  }
+
+ConnectDB();
 
 const app = express();
 
@@ -8,5 +24,6 @@ app.use(express.urlencoded({extended: false}));
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
+
 
 module.exports = app;
