138faf053c7752ea04fe862517f1faa6f22506a8
evelyn <alrxltkfkd@gmail.com>
2022-11-25 16:32:45 +0900

    [FEAT] sequelize blog version

--- Back/src/db/index.js
+++ Back/src/db/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+const User = require("./schemas/user");
 
 const sequelize = new Sequelize(
   "elicewebproject3",
@@ -11,18 +12,15 @@ const sequelize = new Sequelize(
   }
 );
 
-sequelize.authenticate().then(() => {
+const db = {}
+
+db.sequelize = sequelize;
+db.User = User;
+
+User.init(sequelize);
+
+sequelize.sync({ force: false }).then(() => {
     console.log('✅Connection has been established successfully');
  }).catch((error) => {
     console.error('⛔Unable to connect to the database: ', error);
  });
-
-let Dummy = sequelize.define('dummy', {
-    description: Sequelize.STRING
-});
-
-Dummy.sync().then(() => {
-    console.log('New table created');
-}).finally(() => {
-    sequelize.close();
-})
