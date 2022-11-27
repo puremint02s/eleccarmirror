ee46e69b15d6527243b14baf6a140ad97b05912b
haleychoi <chldudgus617@gmail.com>
2022-11-27 22:07:26 +0900

    Feat: MBTI 테스트 로직 구현

---
+++ front/src/Pages/CarMbtiPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CarMbti() {
+  return (
+    <div>
+      <p>전기차 성향 테스트</p>
+      <p>성향을 파악하여 전기차 추천을 해드리겠습니다.</p>
+      <a href="/test">
+        <button>테스트 시작하기</button>
+      </a>
+    </div>
+  );
+}
+
+export default CarMbti;
