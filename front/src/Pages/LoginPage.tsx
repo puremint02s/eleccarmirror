a5f1fb41575cec88839d84104d2044a33824bb55
jun <ryujiyun93@gmail.com>
2022-11-29 23:56:47 +0900

    feat: 게시판 글쓰기 생성

--- front/src/Pages/LoginPage.tsx
+++ front/src/Pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import LoginForm from "../User/LoginForm";
-import LoginHook from "../User/LoginHook";
+import LoginForm from "../components/User/LoginForm";
+import LoginHook from "../components/User/LoginHook";
 
 function LoginPage() {
   const { register, errors, handleSubmit, handleLoginSubmit } = LoginHook();
