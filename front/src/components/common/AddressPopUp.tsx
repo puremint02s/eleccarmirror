d5bfe96d2275dfd6f25089ef0eb9000772c1ab1d
euno <chonist@gmail.com>
2022-11-29 10:16:18 +0900

    [DESIGN] address popup css update

--- front/src/components/common/AddressPopUp.tsx
+++ front/src/components/common/AddressPopUp.tsx
@@ -1,38 +1,55 @@
 import React from "react";
 import DaumPostcode from "react-daum-postcode";
 
+interface address {
+  address: string;
+  addressType: string;
+  bname: string;
+  buildingName: string;
+  zonecode: string;
+}
+
 const AddressPopUp = () => {
-  const complete = (data: object) => {
-    // let fullAddress = data.address;
-    // let extraAddress = '';
+  const setAddress = (data: address) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
     console.log(data);
-    // if (data.addressType === 'R') {
-    //     if (data.bname !== '') {
-    //         extraAddress += data.bname;
-    //     }
-    //     if (data.buildingName !== '') {
-    //         extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
-    //     }
-    //     fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
-    // }
-    // console.log(data)
-    // console.log(fullAddress)
-    // console.log(data.zonecode)
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+    console.log(data);
+    console.log(fullAddress);
+    console.log(data.zonecode);
   };
   return (
     <>
       <div
         style={{
-          width: "300px",
-          height: "500px",
-          zIndex: "9999",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          // -webkit-transform: translate(-50%, -50%);
+          // -moz-transform: translate(-50%, -50%);
+          // -ms-transform: translate(-50%, -50%);
+          // -o-transform: translate(-50%, -50%);
+          transform: "translate(-50%, -50%)",
+          width: "500px",
+          height: "700px",
+          // zIndex: "9999",
+          // display: "flex",
+          // justifyContent: "center",
+          // alignItems: "center",
         }}
       >
         <DaumPostcode
-          onComplete={complete}
+          onComplete={setAddress}
           //style={addressStyle}
           //height={700}
         />
