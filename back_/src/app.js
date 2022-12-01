dfef484a10578c1ee709c0c2677fba778cc8ced6
jun <ryujiyun93@gmail.com>
2022-12-01 17:51:04 +0900

    Fix: create => save() 로 바꿔서 작업중

--- back_/src/app.js
+++ back_/src/app.js
@@ -3,8 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import { userRouter } from "./routers/userRouter.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import { UserModel } from "./db/schemas/user.js";
-import { v4 as uuidv4 } from "uuid";
+import BodyParser from "body-parser";
 
 const app = express();
 
@@ -13,25 +12,11 @@ const PORT = 4003;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(BodyParser.urlencoded({ extended: false }));
 
 app.use(userRouter);
 
 app.use(errorMiddleware);
-
-// app.post("/user/register", async (req, res) => {
-//     const { email, nickname, password } = req.body;
-
-//     const user_id = uuidv4();
-
-//     const createdNewUser = await UserModel.create({
-//         user_id,
-//         email,
-//         nickname,
-//         password,
-//     });
-
-//     return res.json({ createdNewUser });
-// });
 
 app.get("/", (req, res) => {
     return res.send("hi?");
--- back_/src/db/schemas/user.js
+++ back_/src/db/schemas/user.js
@@ -1,50 +1,25 @@
-import mongoose, { Schema, model } from "mongoose";
+import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema(
     {
         user_id: {
             type: String,
-            // required: true,
         },
         email: {
             type: String,
-            // required: true,
         },
         nickname: {
             type: String,
-            // required: true,
         },
         password: {
             type: String,
-            // required: true,
         },
-        // age: {
-        //     type: Number,
-        // },
-        // address: {
-        //     type: String,
-        // },
-        // car_owned: {
-        //     type: Boolean,
-        // },
-        // elec_car_owned: {
-        //     type: Boolean,
-        // },
-        // test_type: {
-        //     type: Number,
-        // },
-        // car_id: {
-        //     type: Number,
-        // },
-        // average_mileage: {
-        //     type: String,
-        // },
     },
     {
         timestamps: true,
     }
 );
 
-const UserModel = model("User", UserSchema);
+const UserModel = model("user", UserSchema);
 
 export { UserModel };
