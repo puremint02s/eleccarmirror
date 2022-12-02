d1603402bc5c0f31466e2785875c5180bbe1740f
euno <eunochoi>
2022-12-02 09:57:33 +0900

    [FEAT] mainPage 1차 완료

---
+++ front/src/components/Main/UserWelcome.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+interface propsTypes {
+  userName: string;
+}
+
+const UserWelcome = ({ userName }: propsTypes) => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "end",
+          width: "100%",
+          height: "100%",
+          padding: "30px",
+          boxSizing: "border-box",
+        }}
+      >
+        <div style={{ fontSize: "2.2em", fontWeight: "600" }}>
+          안녕하세요, <span style={{ color: "#0A84FF" }}>{userName}</span>님!
+        </div>
+        <div
+          style={{
+            fontSize: "1.2em",
+            color: "#898989",
+            margin: "50px 0 20px 0",
+            fontWeight: "600",
+          }}
+        >
+          최근의 주유 하셨나요?
+        </div>
+        <button
+          style={{
+            fontSize: "0.9em",
+            color: "white",
+            backgroundColor: "#898989",
+            border: "none",
+            borderRadius: "50px",
+            padding: "8px 15px",
+            width: "160px",
+            fontWeight: "600",
+          }}
+        >
+          주유 기록 하러 가기
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default UserWelcome;
