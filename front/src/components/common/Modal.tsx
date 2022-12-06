3677f2a4d18913096d0a8957426219571fc318dc
haleychoi <chldudgus617@gmail.com>
2022-12-06 15:26:56 +0900

    Fix: 상세 설명 추가

--- front/src/components/common/Modal.tsx
+++ front/src/components/common/Modal.tsx
@@ -30,10 +30,9 @@ const ModalOutside = styled.div`
 
 const ModalBody = styled.div`
   position: absolute;
-  width: 700px;
-  height: 500px;
+  width: 900px;
+  height: 600px;
   padding: 40px;
-  text-align: center;
   background-color: rgb(255, 255, 255);
   border-radius: 10px;
   box-shadow: 0 2px 3px 0 rgba(34, 36, 38, 0.15);
