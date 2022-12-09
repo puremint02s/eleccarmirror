d4fb3af3d2732c9c501547bfff8db9a4007c96a7
jun <ryujiyun93@gmail.com>
2022-12-09 21:32:32 +0900

    중간저장

--- front/src/Pages/Community/CommunityLoad.tsx
+++ front/src/Pages/Community/CommunityLoad.tsx
@@ -6,38 +6,52 @@ import Main from "components/common/Main";
 import MyInfo from "components/Community/CommunityMyInfo";
 import * as C from "style/CommunityLoadStyle";
 
+type contentProps = {
+  title: string;
+  content: string;
+  createdAt: string;
+  hashtags: [];
+  nickname: string;
+  _id: string;
+  user_id: string;
+};
+
+type userProps = {
+  nickname: string;
+  user_id: string;
+};
+
+type commentProps = {
+  content: string;
+  nickname: string;
+};
+
 function CommunityLoad() {
   const location = useLocation();
+  const navigate = useNavigate();
   const commentRef = useRef<HTMLTextAreaElement>(null);
-  const [contents, setContents] = useState<{
-    title: string;
-    content: string;
-    createdAt: string;
-    hashtags: [];
-    nickname: string;
-    _id: string;
-  } | null>(null);
-
-  const [user, setUser] = useState<{ nickname: string }>();
+  const [contents, setContents] = useState<contentProps | null>(null);
+  const [user, setUser] = useState<userProps>();
   const [comment, setComment] = useState("");
-  const [commentList, setCommentList] = useState<
-    { content: string; nickname: string }[] | []
-  >([]);
+  const [commentList, setCommentList] = useState<commentProps[] | []>([]);
 
   const id = location.state.id;
 
-  // console.log("id ===>", id);
   const baseUrl = "http://localhost:4005";
+  const BearerString =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjhiODVjMzEtOTMzNy00ODU1LWFlZjctZmQzZTMzMWM5YzVjIiwiaWF0IjoxNjcwNTU1NjQ1fQ.g5z1XHSMydzzfP8sXuS27IRolC-dez13OqoUiZdz7pc";
+
   useEffect(() => {
     try {
       axios({
         method: "get",
         headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNGRjY2I5YjQtZDk1OC00ZGNlLThiYzUtZDc2OGViZWNhOTU5IiwiaWF0IjoxNjcwNDc1MjU2fQ.DA0qvxxafWybGMBUSHONTq-dYgXyd9-IcoJnRzTg8zE`,
+          Authorization: `Bearer ${BearerString}`,
         },
         url: `${baseUrl}/community/${id}`,
       }).then(res => {
         setContents(res.data);
+        // console.log("커뮤니티가 가진 유저 아이디", res.data.user_id);
       });
     } catch (err) {
       console.log("err=>", err);
@@ -47,11 +61,11 @@ function CommunityLoad() {
       axios({
         method: "get",
         headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjhiODVjMzEtOTMzNy00ODU1LWFlZjctZmQzZTMzMWM5YzVjIiwiaWF0IjoxNjcwNTU1NjQ1fQ.g5z1XHSMydzzfP8sXuS27IRolC-dez13OqoUiZdz7pc`,
+          Authorization: `Bearer ${BearerString}`,
         },
         url: `${baseUrl}/user/current`,
       }).then(res => {
-        console.log("유저 데이터ㅏ", res.data.nickname);
+        // console.log("현재 유저의 유저 아이디", res.data.user_id);
         setUser(res.data);
       });
     } catch (err) {
@@ -62,11 +76,11 @@ function CommunityLoad() {
       axios({
         method: "get",
         headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjhiODVjMzEtOTMzNy00ODU1LWFlZjctZmQzZTMzMWM5YzVjIiwiaWF0IjoxNjcwNTU1NjQ1fQ.g5z1XHSMydzzfP8sXuS27IRolC-dez13OqoUiZdz7pc`,
+          Authorization: `Bearer ${BearerString}`,
         },
         url: `${baseUrl}/community/comment/${id}`,
       }).then(res => {
-        console.log("해당 커뮤니티 댓글들???", res.data);
+        // console.log("해당 커뮤니티 댓글들???", res.data);
         setCommentList(res.data);
       });
     } catch (err) {
@@ -83,15 +97,28 @@ function CommunityLoad() {
         community_id: contents?._id,
       },
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjhiODVjMzEtOTMzNy00ODU1LWFlZjctZmQzZTMzMWM5YzVjIiwiaWF0IjoxNjcwNTU1NjQ1fQ.g5z1XHSMydzzfP8sXuS27IRolC-dez13OqoUiZdz7pc`,
+        Authorization: `Bearer ${BearerString}`,
       },
       url: `${baseUrl}/comment`,
     }).then(res => {
-      console.log("댓글??", res.data);
+      // console.log("댓글??", res.data);
       setComment(res.data);
-
-      // commentRef?.current?.value = "";
     });
+  };
+
+  const removeContent = () => {
+    axios({
+      method: "delete",
+      data: contents?._id,
+      headers: {
+        Authorization: `Bearer ${BearerString}`,
+      },
+      url: `${baseUrl}/community`,
+    }).then(res => {
+      console.log("삭제?", res.status);
+    });
+
+    navigate(`/community`);
   };
 
   return (
@@ -106,7 +133,17 @@ function CommunityLoad() {
                 <h2>{contents?.title}</h2>
                 {contents?.createdAt?.substring(0, 10)}
               </div>
-              <div>{contents?.nickname}</div>
+              <div>
+                <p>{contents?.nickname}</p>
+                {contents?.user_id === user?.user_id ? (
+                  <p>
+                    <button type="button">수정</button>
+                    <button type="button" onClick={removeContent}>
+                      삭제
+                    </button>
+                  </p>
+                ) : null}
+              </div>
             </C.Title>
             <C.Content>{contents?.content}</C.Content>
             <C.Hashtags>
