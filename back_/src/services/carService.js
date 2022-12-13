2ce5550b79d4875b22cd7bc649b703cee8eb1744
jun <ryujiyun93@gmail.com>
2022-12-13 22:30:37 +0900

    Fix: 차량 생성 제한 삭제 및 같은 유저가 생성한 차량 한번에 수정

--- back_/src/services/carService.js
+++ back_/src/services/carService.js
@@ -2,16 +2,16 @@ import { Car } from "../db/model/Car.js";
 
 class carService {
     static async addCar(newData) {
-        const { user_id } = newData;
+        // const { user_id } = newData;
 
-        const isAleadyCreated = await Car.findCar(user_id);
+        // const isAleadyCreated = await Car.findCar(user_id);
 
-        if (isAleadyCreated) {
-            const errorMessage =
-                "해당 유저는 챠량 등록 요청이 이미 진행되었습니다. 수정 요청을 해주세요";
+        // if (isAleadyCreated) {
+        //     const errorMessage =
+        //         "해당 유저는 챠량 등록 요청이 이미 진행되었습니다. 수정 요청을 해주세요";
 
-            return { errorMessage };
-        }
+        //     return { errorMessage };
+        // }
 
         const newCar = await Car.create(newData);
 
