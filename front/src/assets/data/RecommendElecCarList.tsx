96a85899002b49cb83d7d1c1ae1192ed05d2bbc1
haleychoi <chldudgus617@gmail.com>
2022-12-15 03:27:27 +0900

    Fix: 연비별 추천차량 이미지 업로드 / 최종 결과 페이지 이미지 경로 프롭스 추가

--- front/src/assets/data/RecommendElecCarList.tsx
+++ front/src/assets/data/RecommendElecCarList.tsx
@@ -1,3 +1,35 @@
+import AudiEtronGTImg from "assets/data/CarImages/Audi e-tron GT.png";
+import AudiEtronSSportbackImg from "assets/data/CarImages/Audi e-tron S sportback.png";
+import AudiEtronSImg from "assets/data/CarImages/Audi e-tron S.png";
+import AudiEtronSportbackImg from "assets/data/CarImages/Audi e-tron Sportback.jpg";
+import AudiEtronImg from "assets/data/CarImages/Audi e-tron.jpg";
+import AudiQ4EtronImg from "assets/data/CarImages/Audi Q4 e-tron.png";
+import AudiQ4SportbackEtronImg from "assets/data/CarImages/Audi Q4 Sportback e-tron.jpg";
+import BenzEQASUVImg from "assets/data/CarImages/Benz EQA SUV.png";
+import BenzEQBSUVImg from "assets/data/CarImages/Benz EQB SUV.jpg";
+import BenzEQESedanImg from "assets/data/CarImages/Benz EQE Sedan.jpg";
+import BenzEQSSedanImg from "assets/data/CarImages/Benz EQS Sedan.png";
+import BMWi4M50Img from "assets/data/CarImages/BMW i4 M50.jpg";
+import BMWi4Img from "assets/data/CarImages/BMW i4.jpg";
+import BMWi7Img from "assets/data/CarImages/BMW i7.png";
+import BMWiXM60Img from "assets/data/CarImages/BMW iX M60.png";
+import BMWiXImg from "assets/data/CarImages/BMW iX.jpg";
+import BMWiX3Img from "assets/data/CarImages/BMW iX3.png";
+import ChevroletBoltEvImg from "assets/data/CarImages/Chevrolet BOLT EV.png";
+import GenesisG80Img from "assets/data/CarImages/Genesis G80.png";
+import GenesisGV60Img from "assets/data/CarImages/Genesis GV60.png";
+import GenesisGV70Img from "assets/data/CarImages/Genesis GV70.png";
+import HyundaiIoniq5Img from "assets/data/CarImages/Hyundai Ioniq 5.png";
+import HyundaiIoniq6Img from "assets/data/CarImages/Hyundai Ioniq 6.png";
+import KiaEV6GTImg from "assets/data/CarImages/Kia EV6 GT.png";
+import KiaEV6Img from "assets/data/CarImages/Kia EV6.png";
+import KiaNiroEVImg from "assets/data/CarImages/Kia Niro EV.png";
+import KiaNiroPlusImg from "assets/data/CarImages/Kia Niro Plus.png";
+import TeslaModel3Img from "assets/data/CarImages/Tesla Model 3.jpg";
+import TeslaModelSImg from "assets/data/CarImages/Tesla Model S.jpg";
+import TeslaModelXImg from "assets/data/CarImages/Tesla Model X.png";
+import TeslaModelYImg from "assets/data/CarImages/Tesla Model Y.jpg";
+
 type ResultList = {
   brand: string;
   model: string;
@@ -23,6 +55,7 @@ export const ListA: ResultCarProps = {
     cost: 135600000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/e-tron/audi-e-tron-s.html",
+    img: AudiEtronSImg,
   },
   2: {
     brand: "아우디",
@@ -33,6 +66,7 @@ export const ListA: ResultCarProps = {
     cost: 141482000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/e-tron/audi-e-tron-s-sportback.html",
+    img: AudiEtronSSportbackImg,
   },
   3: {
     brand: "아우디",
@@ -43,6 +77,7 @@ export const ListA: ResultCarProps = {
     cost: 97220000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/e-tron/audi-etron_2021.html",
+    img: AudiEtronImg,
   },
 };
 
@@ -56,6 +91,7 @@ export const ListB: ResultCarProps = {
     cost: 101140000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/e-tron/audi-e-tron-sportback_2021.html",
+    img: AudiEtronSportbackImg,
   },
   2: {
     brand: "BMW",
@@ -66,6 +102,7 @@ export const ListB: ResultCarProps = {
     cost: 155100000,
     homepage:
       "https://www.bmw.co.kr/ko/all-models/m-series/bmw-ix-m60/2021/bmw-ix-m60-highlights.html",
+    img: BMWiXM60Img,
   },
   3: {
     brand: "아우디",
@@ -76,6 +113,7 @@ export const ListB: ResultCarProps = {
     cost: 206300000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/e-tron-gt/etrongt_2022.html",
+    img: AudiEtronGTImg,
   },
 };
 
@@ -89,6 +127,7 @@ export const ListC: ResultCarProps = {
     cost: 148600000,
     homepage:
       "https://www.bmw.co.kr/ko/all-models/bmw-i/bmw-ix/2021/bmw-ix.html",
+    img: BMWiXImg,
   },
   2: {
     brand: "BMW",
@@ -99,6 +138,7 @@ export const ListC: ResultCarProps = {
     cost: 215700000,
     homepage:
       "https://www.bmw.co.kr/ko/all-models/bmw-i/i7/2022/bmw-i7-sedan-highlights.html",
+    img: BMWi7Img,
   },
   3: {
     brand: "아우디",
@@ -109,6 +149,7 @@ export const ListC: ResultCarProps = {
     cost: 143320000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/e-tron-gt/etrongt_2022.html",
+    img: AudiEtronGTImg,
   },
   4: {
     brand: "kia",
@@ -118,6 +159,7 @@ export const ListC: ResultCarProps = {
     MPG: 3.9,
     cost: 76680000,
     homepage: "https://www.kia.com/kr/vehicles/ev6-gt/features",
+    img: KiaEV6GTImg,
   },
 };
 
@@ -131,6 +173,7 @@ export const ListD: ResultCarProps = {
     cost: 188600000,
     homepage:
       "https://www.mercedes-benz.co.kr/passengercars/mercedes-benz-cars/models/eqs/saloon-v297/explore.html",
+    img: BenzEQSSedanImg,
   },
   2: {
     brand: "벤츠",
@@ -141,6 +184,7 @@ export const ListD: ResultCarProps = {
     cost: 59900000,
     homepage:
       "https://www.mercedes-benz.co.kr/passengercars/mercedes-benz-cars/models/eqa/explore.html",
+    img: BenzEQASUVImg,
   },
   3: {
     brand: "BMW",
@@ -151,6 +195,7 @@ export const ListD: ResultCarProps = {
     cost: 84900000,
     homepage:
       "https://www.bmw.co.kr/ko/all-models/m-series/i4-m50/2021/bmw-i4-m50-highlights.html",
+    img: BMWi4M50Img,
   },
   4: {
     brand: "BMW",
@@ -161,6 +206,7 @@ export const ListD: ResultCarProps = {
     cost: 75900000,
     homepage:
       "https://www.bmw.co.kr/ko/all-models/x-series/iX3/2021/bmw-ix3-highlights.html",
+    img: BMWiX3Img,
   },
   5: {
     brand: "테슬라",
@@ -170,6 +216,7 @@ export const ListD: ResultCarProps = {
     MPG: 4.1,
     cost: 129990000,
     homepage: "https://www.tesla.com/ko_kr/models",
+    img: TeslaModelSImg,
   },
   6: {
     brand: "테슬라",
@@ -179,6 +226,7 @@ export const ListD: ResultCarProps = {
     MPG: 4.1,
     cost: 139990000,
     homepage: "https://www.tesla.com/ko_kr/modelx",
+    img: TeslaModelXImg,
   },
   7: {
     brand: "아우디",
@@ -189,6 +237,7 @@ export const ListD: ResultCarProps = {
     cost: 63700000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/q4_e-tron/q4etronsb_2022.html",
+    img: AudiQ4SportbackEtronImg,
   },
   8: {
     brand: "벤츠",
@@ -199,6 +248,7 @@ export const ListD: ResultCarProps = {
     cost: 76000000,
     homepage:
       "https://www.mercedes-benz.co.kr/passengercars/mercedes-benz-cars/models/eqb/suv-x243/explore.html",
+    img: BenzEQBSUVImg,
   },
   9: {
     brand: "제네시스",
@@ -209,6 +259,7 @@ export const ListD: ResultCarProps = {
     cost: 82800000,
     homepage:
       "https://www.genesis.com/kr/ko/models/luxury-sedan-genesis/g80/highlights.html",
+    img: GenesisG80Img,
   },
   10: {
     brand: "아우디",
@@ -219,6 +270,7 @@ export const ListD: ResultCarProps = {
     cost: 59700000,
     homepage:
       "https://www.audi.co.kr/kr/web/ko/models/q4_e-tron/q4etron_2022.html",
+    img: AudiQ4EtronImg,
   },
   11: {
     brand: "벤츠",
@@ -229,6 +281,7 @@ export const ListD: ResultCarProps = {
     cost: 103000000,
     homepage:
       "https://www.mercedes-benz.co.kr/passengercars/mercedes-benz-cars/models/eqe/saloon-v295/explore.html",
+    img: BenzEQESedanImg,
   },
 };
 
@@ -242,6 +295,7 @@ export const ListE: ResultCarProps = {
     cost: 66400000,
     homepage:
       "https://www.bmw.co.kr/ko/all-models/bmw-i/i4/2021/bmw-i4-highlights.html",
+    img: BMWi4Img,
   },
   2: {
     brand: "제네시스",
@@ -252,6 +306,7 @@ export const ListE: ResultCarProps = {
     cost: 73320000,
     homepage:
       "https://www.genesis.com/kr/ko/models/luxury-suv-genesis/gv70/highlights.html",
+    img: GenesisGV70Img,
   },
   3: {
     brand: "테슬라",
@@ -261,6 +316,7 @@ export const ListE: ResultCarProps = {
     MPG: 4.8,
     cost: 96640000,
     homepage: "https://www.tesla.com/ko_kr/modely",
+    img: TeslaModelYImg,
   },
 };
 
@@ -273,6 +329,7 @@ export const ListF: ResultCarProps = {
     MPG: 5.1,
     cost: 70340000,
     homepage: "https://www.tesla.com/ko_kr/model3",
+    img: TeslaModel3Img,
   },
   2: {
     brand: "제네시스",
@@ -283,6 +340,7 @@ export const ListF: ResultCarProps = {
     cost: 82800000,
     homepage:
       "https://www.genesis.com/kr/ko/models/luxury-sedan-genesis/g80/highlights.html",
+    img: GenesisGV60Img,
   },
   3: {
     brand: "현대",
@@ -292,6 +350,7 @@ export const ListF: ResultCarProps = {
     MPG: 5.2,
     cost: 50050000,
     homepage: "https://www.hyundai.com/kr/ko/e/vehicles/ioniq5/intro",
+    img: HyundaiIoniq5Img,
   },
   4: {
     brand: "기아",
@@ -301,6 +360,7 @@ export const ListF: ResultCarProps = {
     MPG: 5.3,
     cost: 49420000,
     homepage: "https://www.kia.com/kr/vehicles/niro-plus/features",
+    img: KiaNiroEVImg,
   },
   5: {
     brand: "기아",
@@ -310,6 +370,7 @@ export const ListF: ResultCarProps = {
     MPG: 5.3,
     cost: 44200000,
     homepage: "https://www.kia.com/kr/vehicles/niro-plus/features",
+    img: KiaNiroPlusImg,
   },
 };
 
@@ -322,6 +383,7 @@ export const ListG: ResultCarProps = {
     MPG: 5.6,
     cost: 51870000,
     homepage: "https://www.kia.com/kr/vehicles/ev6/features",
+    img: KiaEV6Img,
   },
 };
 
@@ -334,6 +396,7 @@ export const ListH: ResultCarProps = {
     MPG: 6.2,
     cost: 52000000,
     homepage: "https://www.hyundai.com/kr/ko/e/vehicles/ioniq6/intro",
+    img: HyundaiIoniq6Img,
   },
 };
 
@@ -346,5 +409,6 @@ export const ListI: ResultCarProps = {
     MPG: 6.6,
     cost: 44300000,
     homepage: "https://www.chevrolet.co.kr/electric/bolt-ev",
+    img: ChevroletBoltEvImg,
   },
 };
