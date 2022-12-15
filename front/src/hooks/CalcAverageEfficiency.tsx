2984783c817c4faf3a503a9b6a5800dc743485c3
haleychoi <chldudgus617@gmail.com>
2022-12-15 20:06:15 +0900

    Fix: 평균 연비 계산식 수정

--- front/src/hooks/CalcAverageEfficiency.tsx
+++ front/src/hooks/CalcAverageEfficiency.tsx
@@ -3,23 +3,24 @@ import { getUserRefuelRecord } from "apis/RefuelRecordApi";
 
 function CalcAverageEfficiency(currentUserId: string) {
   const [firstAmount, setFirstAmount] = useState(0);
-  const [secondAmount, setSecondAmount] = useState(0);
+  const [firstOdometer, setFirstOdometer] = useState(0);
   const [secondOdometer, setSecondOdometer] = useState(0);
 
   useEffect(() => {
     async function getRefuelRecord(currentUserId: string) {
       const res = await getUserRefuelRecord(currentUserId);
-      // console.log(res);
       if (res) {
         setFirstAmount(res[res.length - 2].gas_amount);
-        setSecondAmount(res[res.length - 1].gas_amount);
+        setFirstOdometer(res[res.length - 2].odometer);
         setSecondOdometer(res[res.length - 1].odometer);
       }
     }
     getRefuelRecord(currentUserId);
   }, []);
 
-  const calcResult = (secondOdometer / (secondAmount - firstAmount)).toFixed(2);
+  const calcResult = ((secondOdometer - firstOdometer) / firstAmount).toFixed(
+    2,
+  );
   const intCalcResult = parseInt(calcResult);
   const averageEfficiency = Math.abs(intCalcResult);
 
