21de2feea4f32db75a316f13abe5204662554e8a
euno <chonist@gmail.com>
2022-12-16 04:49:14 +0900

    fix, 이미지 업로드 요청  react query 로 변경

--- front/src/apis/ImageUpload.tsx
+++ front/src/apis/ImageUpload.tsx
@@ -1,6 +1,19 @@
 import { axiosInstance, axiosLoginInstance } from "./AxiosInstance";
+import axios from "axios";
+
+const BASE_URL =
+  "http://" +
+  window.location.hostname +
+  ":" +
+  process.env.REACT_APP_BACK_SERVER_PORT +
+  "/";
+
+interface carData {
+  filename: string;
+  prediction: Array<number>;
+}
 
 export const postImage = async (image: any) => {
-  const res = await axiosInstance.post("/images", image);
-  return res;
+  const { data } = await axios.post(`${BASE_URL}images`, image);
+  return data;
 };
